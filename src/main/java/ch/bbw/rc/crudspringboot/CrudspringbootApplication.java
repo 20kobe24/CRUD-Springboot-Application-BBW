3cc93ab051cf5e5ae07d62244c6eb95f2b8337b8
Ryan <ryan.carpio@hotmail.com>
2023-01-24 21:43:41 +0100

    base springboot project

---
+++ src/main/java/ch/bbw/rc/crudspringboot/CrudspringbootApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.rc.crudspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/rc/crudspringboot/CrudspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.rc.crudspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
