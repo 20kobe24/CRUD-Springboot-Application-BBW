240a428c75a479be9aba63f46d72e16541c8e511
Ryan <ryan.carpio@hotmail.com>
2023-01-25 20:05:44 +0100

    created get all in felfelprodukt service

--- src/main/java/ch/bbw/rc/crudspringboot/Service/FelfelProduktService.java
+++ src/main/java/ch/bbw/rc/crudspringboot/Service/FelfelProduktService.java
@@ -1,7 +1,10 @@
 package ch.bbw.rc.crudspringboot.Service;
 
+import ch.bbw.rc.crudspringboot.model.FelfelProdukt;
 import ch.bbw.rc.crudspringboot.repository.FelfelProduktRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FelfelProduktService {
@@ -11,4 +14,10 @@ public class FelfelProduktService {
     public FelfelProduktService(FelfelProduktRepository felfelProduktRepository) {
         this.felfelProduktRepository = felfelProduktRepository;
     }
+
+    public List<FelfelProdukt> getAllFelfelProdukts() {
+        return felfelProduktRepository.findAll();
+    }
+
+
 }
