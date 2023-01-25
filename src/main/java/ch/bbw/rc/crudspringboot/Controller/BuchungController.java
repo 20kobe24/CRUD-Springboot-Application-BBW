545af0f235f644722cca6b55ae97f5ad4b64ad00
Ryan <ryan.carpio@hotmail.com>
2023-01-25 20:33:40 +0100

    add: get ALL Buchungen implemented

--- src/main/java/ch/bbw/rc/crudspringboot/Controller/BuchungController.java
+++ src/main/java/ch/bbw/rc/crudspringboot/Controller/BuchungController.java
@@ -1,4 +1,26 @@
 package ch.bbw.rc.crudspringboot.Controller;
 
+
+import ch.bbw.rc.crudspringboot.Service.BuchungService;
+import ch.bbw.rc.crudspringboot.model.Buchung;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/buchungen")
 public class BuchungController {
+
+    @Autowired
+    private BuchungService buchungService;
+
+    @GetMapping
+    public List<Buchung> getAllBuchungen() {
+        return buchungService.getAllBuchungen();
+    }
+
+
 }
--- src/main/java/ch/bbw/rc/crudspringboot/Service/BuchungService.java
+++ src/main/java/ch/bbw/rc/crudspringboot/Service/BuchungService.java
@@ -1,4 +1,23 @@
 package ch.bbw.rc.crudspringboot.Service;
 
+import ch.bbw.rc.crudspringboot.model.Buchung;
+import ch.bbw.rc.crudspringboot.repository.BuchungRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BuchungService {
+
+    @Autowired
+    private final BuchungRepository buchungRepository;
+
+    public BuchungService(BuchungRepository buchungRepository) {
+        this.buchungRepository = buchungRepository;
+    }
+
+    public List<Buchung> getAllBuchungen() {
+        return buchungRepository.findAll();
+    }
 }
