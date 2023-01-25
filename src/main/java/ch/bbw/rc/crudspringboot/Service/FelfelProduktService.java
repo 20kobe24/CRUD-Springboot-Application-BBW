876e5bf69a025d963eeff878d7f2ee39f3272712
Ryan <ryan.carpio@hotmail.com>
2023-01-25 20:04:45 +0100

    implemented repository and created constructor for felfelprodukt

--- src/main/java/ch/bbw/rc/crudspringboot/Service/FelfelProduktService.java
+++ src/main/java/ch/bbw/rc/crudspringboot/Service/FelfelProduktService.java
@@ -1,4 +1,14 @@
 package ch.bbw.rc.crudspringboot.Service;
 
+import ch.bbw.rc.crudspringboot.repository.FelfelProduktRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FelfelProduktService {
+
+    private final FelfelProduktRepository felfelProduktRepository;
+
+    public FelfelProduktService(FelfelProduktRepository felfelProduktRepository) {
+        this.felfelProduktRepository = felfelProduktRepository;
+    }
 }
