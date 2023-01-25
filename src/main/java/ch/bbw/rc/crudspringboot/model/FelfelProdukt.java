c6b90bb748887fd446de9b025b286ef35aa86faa
Ryan <ryan.carpio@hotmail.com>
2023-01-25 20:20:35 +0100

    fix: autoincrement in felfelprodukt model

--- src/main/java/ch/bbw/rc/crudspringboot/model/FelfelProdukt.java
+++ src/main/java/ch/bbw/rc/crudspringboot/model/FelfelProdukt.java
@@ -1,9 +1,6 @@
 package ch.bbw.rc.crudspringboot.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,12 +13,13 @@ import lombok.NoArgsConstructor;
 public class FelfelProdukt {
     @Id
     @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "price")
     private Float price;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "name")
     private String name;
 
 
