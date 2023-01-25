4f6115d2c19e9bdf73b64c6708e2b33c3b069def
Ryan <ryan.carpio@hotmail.com>
2023-01-25 20:06:16 +0100

    created getbyid in felfelprodukt service

--- src/main/java/ch/bbw/rc/crudspringboot/Service/FelfelProduktService.java
+++ src/main/java/ch/bbw/rc/crudspringboot/Service/FelfelProduktService.java
@@ -19,5 +19,8 @@ public class FelfelProduktService {
         return felfelProduktRepository.findAll();
     }
 
+    public FelfelProdukt getFelfelProduktById(Long id) {
+        return felfelProduktRepository.findById(id).orElse(null);
+    }
 
 }
